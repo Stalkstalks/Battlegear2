63c0422792e231b675280bec0a7ee71325ae56b9
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-09-18 17:02:29 +0200

    setup mixins environment

--- src/main/java/mods/battlegear2/coremod/BattlegearLoadingPlugin.java
+++ src/main/java/mods/battlegear2/coremod/BattlegearLoadingPlugin.java
@@ -1,8 +1,14 @@
 package mods.battlegear2.coremod;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import com.gtnewhorizon.gtnhmixins.IEarlyMixinLoader;
+
+import cpw.mods.fml.relauncher.FMLLaunchHandler;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.Name;
@@ -14,7 +20,7 @@ import mods.battlegear2.api.core.BattlegearTranslator;
 @Name("Mine and Blade: Battlegear2")
 @SortingIndex(1500)
 @MCVersion("1.7.10")
-public final class BattlegearLoadingPlugin implements IFMLLoadingPlugin {
+public final class BattlegearLoadingPlugin implements IEarlyMixinLoader, IFMLLoadingPlugin {
 
     public static final String EntityPlayerTransformer = "mods.battlegear2.coremod.transformers.EntityPlayerTransformer";
     public static final String ModelBipedTransformer = "mods.battlegear2.coremod.transformers.ModelBipedTransformer";
@@ -58,6 +64,19 @@ public final class BattlegearLoadingPlugin implements IFMLLoadingPlugin {
     @Override
     public void injectData(Map<String, Object> data) {
         debugOutputLocation = new File(data.get("mcLocation").toString(), "bg edited classes");
-        BattlegearTranslator.obfuscatedEnv = Boolean.class.cast(data.get("runtimeDeobfuscationEnabled"));
+        BattlegearTranslator.obfuscatedEnv = (boolean) data.get("runtimeDeobfuscationEnabled");
     }
+
+    @Override
+    public String getMixinConfig() {
+        return "mixins.battlegear2.early.json";
+    }
+
+    @Override
+    public List<String> getMixins(Set<String> loadedCoreMods) {
+        final List<String> mixins = new ArrayList<>();
+        if (FMLLaunchHandler.side().isClient()) {}
+        return mixins;
+    }
+
 }
