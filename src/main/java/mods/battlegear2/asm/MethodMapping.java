762a439eb334d0899a34ddee07625690de673685
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-10-10 01:38:22 +0200

    spotless

--- src/main/java/mods/battlegear2/asm/MethodMapping.java
+++ src/main/java/mods/battlegear2/asm/MethodMapping.java
@@ -4,21 +4,16 @@ import mods.battlegear2.asm.loader.BattlegearLoadingPlugin;
 
 public enum MethodMapping {
 
+    // spotless:off
     ENTITYAICONTROLLEDBYPLAYER$UPDATETASK("e()V", "updateTask()V", 1),
-    ENTITYOTHERPLAYERMP$SETCURRENTITEMORARMOR("c(ILadd;)V", "setCurrentItemOrArmor(ILnet/minecraft/item/ItemStack;)V",
-            1),
+    ENTITYOTHERPLAYERMP$SETCURRENTITEMORARMOR("c(ILadd;)V", "setCurrentItemOrArmor(ILnet/minecraft/item/ItemStack;)V", 1),
     ENTITYPLAYER$SETCURRENTITEMORARMOR("c(ILadd;)V", "setCurrentItemOrArmor(ILnet/minecraft/item/ItemStack;)V", 1),
-    ITEMINWORLDMANAGER$TRYUSEITEM("a(Lyz;Lahb;Ladd;)Z",
-            "tryUseItem(Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;)Z",
-            2),
+    ITEMINWORLDMANAGER$TRYUSEITEM("a(Lyz;Lahb;Ladd;)Z", "tryUseItem(Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;)Z", 2),
     MINECRAFT$RIGHTCLICKMOUSE("am()V", "func_147121_ag()V", 1),
-    NETHANDLERPLAYCLIENT$HANDLESPAWNPLAYER("a(Lgb;)V",
-            "handleSpawnPlayer(Lnet/minecraft/network/play/server/S0CPacketSpawnPlayer;)V", 2),
-    NETHANDLERPLAYSERVER$PROCESSPLAYERBLOCKPLACEMENT("a(Ljo;)V",
-            "processPlayerBlockPlacement(Lnet/minecraft/network/play/client/C08PacketPlayerBlockPlacement;)V", 2),
-    PLAYERCONTROLLERMP$SENDUSEITEM("a(Lyz;Lahb;Ladd;)Z",
-            "sendUseItem(Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;)Z",
-            2);
+    NETHANDLERPLAYCLIENT$HANDLESPAWNPLAYER("a(Lgb;)V", "handleSpawnPlayer(Lnet/minecraft/network/play/server/S0CPacketSpawnPlayer;)V", 2),
+    NETHANDLERPLAYSERVER$PROCESSPLAYERBLOCKPLACEMENT("a(Ljo;)V", "processPlayerBlockPlacement(Lnet/minecraft/network/play/client/C08PacketPlayerBlockPlacement;)V", 2),
+    PLAYERCONTROLLERMP$SENDUSEITEM("a(Lyz;Lahb;Ladd;)Z", "sendUseItem(Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;)Z", 2);
+    // spotless:on
 
     private final String obfMapping;
     public final String clearMapping;
